ce0bcce2396a43b4a7a3fb2d86f4da040105a2d9
o-dim <omiia0407@gmail.com>
2023-08-09 14:35:39 +0900

    day3

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 const express = require('express');
 const app = express();
 app.use(express.urlencoded({extended: true})) 
-
+app.set('view engine', 'ejs');
 var db;
 // mongoDB 연결
 const MongoClient = require('mongodb').MongoClient;
 const mongoURL = 'mongodb+srv://admin:admin123@cluster.0d1qwnl.mongodb.net/?retryWrites=true&w=majority';
 MongoClient.connect(mongoURL, function(에러, client) {
-    if (에러) return console.log(에러);
-    // 데이터 저장하기
     db = client.db('todoapp');
-    // 이름과 나이 저장하기
-    db.collection('post').insertOne({이름 : '카리나', 직업 : '에스파', _id : 'aespa1'}, function(에러, 결과) {
-        console.log('저장완료');
-    }); // 생성한 컬렉션 이름에 insert 시킴
-
-
+    if (에러) return console.log(에러);
     app.listen(8080, function () {
         console.log('server start');
     });
@@ -38,18 +31,31 @@ app.get('/write', function(request, response) {
 })
 
 app.post('/add', (request, response) => {
-    MongoClient.connect(mongoURL, function(에러, client) {
-        if (에러) return console.log(에러);
-        // 데이터 저장하기
-        db = client.db('todoapp');
-        // 이름과 나이 저장하기
-        db.collection('post').insertOne({날짜 : request.body.date, 제목 : request.body.title, _id : 'todo1'}, function(에러, 결과) {
-            console.log('저장완료');
-        }); // 생성한 컬렉션 이름에 insert 시킴
-    
-    
-        app.listen(8080, function () {
-            console.log('server start');
+    response.send('전송 완료');
+    db.collection('counter').findOne({name : '게시물개수'}, function(error, result) {
+        console.log(result.totalPost);
+        var 총게시물 = result.totalPost;
+        db.collection('post').insertOne({_id : 총게시물 + 1, 제목 : request.body.title, 날짜 : request.body.date}, function(에러, 결과) {
+            console.log('DB에 저장완료');
+            db.collection('counter').updateOne({name : '게시물개수'}, { $inc : {totalPost : 1}}, function(error, result) {
+                if(error){return console.log(error)}
+            })
         });
+    });
+})
+
+app.get('/list', (request, response) => {
+    db.collection('post').find().toArray(function(error, result) {
+        console.log(result);
+        response.render('list.ejs', {posts : result});
+    });
+})
+
+app.delete('/delete', (request, response) => {
+    console.log(request.body);
+    // _id 숫자로 변환하기
+    request.body._id = parseInt(request.body._id);
+    db.collection('post').deleteOne(request.body, function(error, result) {
+        console.log(error);
     })
 })
